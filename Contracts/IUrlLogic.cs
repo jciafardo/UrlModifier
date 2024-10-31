1b2a5da1a68fdffae9d9888c674253e63a5cf964
JackCiafardo <JackCiafardo@BRIO-0037>
2024-10-31 08:52:51 -0400

    Add project files.

---
+++ Contracts/IUrlLogic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public interface IUrlLogic
+    {
+
+        /// <summary>
+        /// encodes a URL to a shortened URL.
+        /// </summary>
+        /// <param name="url"> Url we want to shorten </param>
+        /// <returns>string of the url that has been shortend</returns>
+        public string EncodeUrl(string url);
+
+
+        /// <summary>
+        /// decodes a shortened URL to its original URL.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns>string of the url that has been lengthend</returns>
+        public string DecodeUrl(string url);
+
+    }
+}
---
+++ UrlModifier/Services/UrlLogic.cs
@@ -0,0 +1,18 @@
+﻿using Contracts;
+
+namespace UrlModifier.Services
+{
+    public class UrlLogic : IUrlLogic
+
+    {
+        public string DecodeUrl(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string EncodeUrl(string url)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
